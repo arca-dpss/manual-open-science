35dc2f12165c54349a8275985b9b044383fe71cf
Claudio Zandonella <claudiozandonella@gmail.com>
2022-04-21 20:32:17 +0200

    first part revision chapter 5 coding

--- R/utils-book.R
+++ R/utils-book.R
@@ -30,20 +30,21 @@ break_line <- function(format = "html"){
 
 #----    package_logo    ----#
 
-package_logo <- function(img, format = "html", tex_width = .25){
+package_logo <- function(img, format = "html", tex_width = .25, h_adjust = -15){
 
   if(format == "html"){
     res <- glue::glue('<div class="packages-logo-box"><img src="{img}"></div>')
   } else {
-    res <- glue::glue(
-    '\\begin{wrapfigure}{r}{^tex_width^\\textwidth}
+    res <- glue::glue('
+\\begin{wrapfigure}{r}{^tex_width^\\textwidth}
+  \\vspace*{^h_adjust^pt}
   \\begin{center}
     \\includegraphics[width=^tex_width-.02^\\textwidth]{^img^}
   \\end{center}
+  \\vspace{^h_adjust^pt}
 \\end{wrapfigure}',.open = "^", .close = "^")
 
   }
   res
 }
-
 

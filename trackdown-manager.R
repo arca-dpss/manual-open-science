cb31bfe272a6b5830656dce064888f9bad525069
Claudio Zandonella <claudiozandonella@gmail.com>
2022-04-22 09:54:21 +0200

    add chapter 05 coding online

--- trackdown-manager.R
+++ trackdown-manager.R
@@ -21,5 +21,11 @@ trackdown::download_file("chapters/03-projects.Rmd", gpath = "trackdown/ARCA")
 trackdown::update_file("chapters/04-data.Rmd", gpath = "trackdown/ARCA")
 trackdown::download_file("chapters/04-data.Rmd", gpath = "trackdown/ARCA")
 
+#----    05-data    ----
+trackdown::update_file("chapters/05-coding.Rmd", gpath = "trackdown/ARCA")
+trackdown::download_file("chapters/05-coding.Rmd", gpath = "trackdown/ARCA")
+
 #----    make_book    ----
 make_my_book()
+
+bookdown::preview_chapter("chapters/05-coding.Rmd", 'bookdown::pdf_book')
